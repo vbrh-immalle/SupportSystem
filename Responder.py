96a26eae470c0df83241fe3ebbe81d2c15e02f8b
vbrh-immalle <hans.vanbroeckhoven@immalle.be>
2018-11-19 11:37:43 +0100

    SupportSystem v2 with randint

--- Responder.py
+++ Responder.py
@@ -1,3 +1,17 @@
+from random import randint
+
 class Responder:
+    
+    def __init__(self):
+        self.responses = []
+        self.fillResponses()
+
     def generateResponse(self):
-        return "Amaj dat is interessant... Vertel me meer..."
+        random_index = randint(0, len(self.responses) - 1)
+        return self.responses[random_index]
+
+    def fillResponses(self):
+        self.responses.append("Ahzo?")
+        self.responses.append("Echt?")
+        self.responses.append("Dat is ongeloofelijk!")
+        self.responses.append("Amaj dat is interessant... Vertel me meer...")
